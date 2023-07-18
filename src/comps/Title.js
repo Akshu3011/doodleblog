bb4d9d7d514075056e752b10e9fabe218e3677df
Akshata Kumbhar <105023659+Akshu3011@users.noreply.github.com>
2023-07-18 16:52:48 -0700

    added background color

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Title() {
   return (
     <div className="title">
-    <h1>Akshu3011</h1>
-    <h2>My Pictures</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <h1>By Akshata</h1>
+    <h2>Doodles3011</h2>
+    <p>Step into my colorful world of cartoon doodles, where imagination takes shape and brings joy to life.</p>
   </div>
   )
 }
